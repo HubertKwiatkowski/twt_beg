a6e3a2a27b1a41d2f8554fd67fae24a93eb810f1
warlock <h.kwiatkowski@hotmail.com>
2022-01-05 19:18:53 +0100

    tut end

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -15,5 +16,3 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
-
-        
--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
+    path("", views.home, name="home"),
     path("home/", views.home, name="home"),
     path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
+
 ]
